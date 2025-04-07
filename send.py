f63ffe19b048f061679e95b21580f24d25efa83a
Lindsay Qin <lindsaymqin@gmail.com>
2025-04-07 12:16:55 -0700

    Sends/receives 2 ints (signed). Can send ints from -32,768 to 32,767. Currently sends 5 times per second -- need to modify python/arduino code if you want to increase.

--- send.py
+++ send.py
@@ -1,24 +1,44 @@
 import asyncio
 from bleak import BleakClient
 import struct
+import random
 
 xiao_address = "26:37:79:5a:8b:e9"
 xiao_address_mac = "E6A89626-3D00-580B-1D80-F613D65739F1"
 xiao_service = "19B10000-E8F2-537E-4F6C-D104768A1214"
-xiao_characteristic = "19B10001-E8F2-537E-4F6C-D104768A1214"
+xiao_read_characteristic = "19B10001-E8F2-537E-4F6C-D104768A1214"
+xiao_write_characteristic = "20B1D001-E8F2-537E-4F6C-A104768A1517"
 
-async def printVal(characteristic, data):
-    print(struct.unpack('<f', data)[0])
+async def printVals(characteristic, data): #callback function must have these parameters
+    val1, val2 = struct.unpack('<hh', data)
+    print("Received: ", val1, val2)
 
+async def sendVals(freq, client, characteristic):
+    sleep_duration = 1/freq
+    
+    for _ in range(freq): 
+        val1 = random.randint(-255, 255)  # Any integer from 0–255
+        val2 = random.randint(-255, 255)
+
+        send_bytes = struct.pack('<hh', val1, val2)
+
+        await client.write_gatt_char(characteristic, send_bytes)
+        print("Sent: ", val1, val2)
+        await asyncio.sleep(sleep_duration)
+    
 async def main():
     print("Trying to connect...")
     
     async with BleakClient(xiao_address_mac) as client:
         print("Connected.")
 
-        await client.start_notify(xiao_characteristic, printVal)
-        await asyncio.sleep(1)
-        await client.stop_notify(xiao_characteristic)
+        await client.start_notify(xiao_read_characteristic, printVals)
+    
+         #send 5/10 times within a second. sending 2 pwm values (0-250)
+        await sendVals(5, client, xiao_write_characteristic)
+            
+        await asyncio.sleep(1) #only sends/receives for a second
+        await client.stop_notify(xiao_read_characteristic)
 
 
 asyncio.run(main())
--- xiao_sense/xiao_sense.ino
+++ xiao_sense/xiao_sense.ino
@@ -3,11 +3,13 @@
 
 BLEService test("19B10000-E8F2-537E-4F6C-D104768A1214");
 BLECharacteristic sendCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify, 4);
+BLECharacteristic receiveCharacteristic("20B1D001-E8F2-537E-4F6C-A104768A1517", BLEWrite | BLENotify, 4);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //baud rate is currently 9600
   while (!Serial);
 
+  Serial.println("Beginning setup...");
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy module failed!");
@@ -20,35 +22,57 @@ void setup() {
 
   // add the characteristic to the service
   test.addCharacteristic(sendCharacteristic);
+  test.addCharacteristic(receiveCharacteristic);
 
   // add service
   BLE.addService(test);
 
   // set initial value for characeristic:
-  float empty = 0.0f;
-  uint8_t buffer[4];
-  memcpy(buffer, &empty, sizeof(empty));
-  sendCharacteristic.writeValue(buffer, 4);
+  int16_t empty[2] = {0};
+  
+  sendCharacteristic.writeValue(empty, 4);
+  receiveCharacteristic.writeValue(empty, 4);
 
   // start advertising
   BLE.advertise();
+  Serial.print("BLE Address: ");
   Serial.println(BLE.address());
 
-  Serial.println("BLE Setup Done");
- 
+  Serial.println("BLE Setup Done!");
 }
 
 void loop() {
   BLE.poll(); //necessary to be able to send values
-  float sendVal = 1.0f;
 
-  uint8_t buffer[4];
-  memcpy(buffer, &sendVal, sizeof(sendVal));
+  if (receiveCharacteristic.written()) { //only sends/receives if computer writes to it
+    //================= send values from arduino ================ 
+    int16_t sendVal1 = random(-250, 251); //can send ints from -32,768 to 32,767
+    int16_t sendVal2 = random(-250, 251);
 
-  Serial.print("write value: ");
-  Serial.println(sendVal);
+    uint8_t buffer1[4];
+    buffer1[0] = sendVal1 & 0xFF;
+    buffer1[1] = (sendVal1 >> 8) & 0xFF;
+    buffer1[2] = sendVal2 & 0xFF;
+    buffer1[3] = (sendVal2 >> 8) & 0xFF;
+    
+    Serial.print("sent value: ");
+    Serial.print(sendVal1);
+    Serial.print(", ");
+    Serial.println(sendVal2);
+    sendCharacteristic.writeValue(buffer1, 4);
 
-  sendCharacteristic.writeValue(buffer, 4);
+    //============ receiving values from computer =================
 
-  delay(1000);
+    uint8_t buffer2[4];
+    receiveCharacteristic.readValue(buffer2, 4);
+    int16_t receiveVal1 = (buffer2[1] << 8) | buffer2[0]; 
+    int16_t receiveVal2 = (buffer2[3] << 8) | buffer2[2];
+
+    Serial.print("received value: ");
+    Serial.print(receiveVal1);
+    Serial.print(", ");
+    Serial.println(receiveVal2);
+
+    delay(200); //waits 200 ms between sends/receives.
+  }
 }
